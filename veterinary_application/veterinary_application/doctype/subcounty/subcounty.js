c7a6facca52a11e7c13c736ed3588c28e8bce38a
howdini <howarduma@gmail.com>
2021-06-17 07:41:04 +0000

    update

---
+++ veterinary_application/veterinary_application/doctype/subcounty/subcounty.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Borealis Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Subcounty', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ veterinary_application/veterinary_application/doctype/ward/ward.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Borealis Systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Ward', {
+	// refresh: function(frm) {
+
+	// }
+});
